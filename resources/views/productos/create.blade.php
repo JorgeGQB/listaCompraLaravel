7f015675e8bfd1c7b0728b3bf1b94df3a4f78d08
JorgeGQB <jgqb26@gmail.com>
2019-11-20 13:42:00 +0100

    Ejercicios Controllers-Vistas

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -2,6 +2,54 @@
 
 @section('content')
 
-    Añadir producto
+<div class="row" style="margin-top:40px">
+    <div class="offset-md-3 col-md-6">
+        <div class="card">
+            <div class="card-header text-center">
+            Añadir producto
+            </div>
+        <div class="card-body" style="padding:30px">
+
+            <form action="#" method="POST">
+
+            @csrf
+
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="price">Precio</label>
+                <input type="number" name="price" id="price" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="category">Categoría</label>
+                <input type="text" name="category" id="category" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="image">Imagen</label>
+                <input type="text" name="image" id="image" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="description">Descripción</label>
+                <input type="textarea" name="description" id="description" class="form-control">
+            </div>
+
+            <div class="form-group text-center">
+                <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                    Añadir producto
+                </button>
+            </div>
+
+            </form>
+
+        </div>
+    </div>
+    </div>
+</div>
 
 @stop
--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
-    Vista detalle producto {{ $id }}
+<div class="row">
+
+    <div class="col-sm-4">
+
+        <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
+
+    </div>
+    <div class="col-sm-8">
+
+        <h2>{{$id[0]}}</h2>
+        <p><b>Categoría:</b> {{$id[1]}}</p>
+        <p><b>Estado: </b>Producto actualmente comprado</p>
+        <br>
+        <button type="button" class="btn btn-danger">Pendiente de compra</button>
+        <button class="btn btn-warning">Editar producto</button>
+        <button class="btn btn-default">Volver al índice</button>
+
+    </div>
+</div>
+
 
 @stop
